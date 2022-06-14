b8cbc782f2ab57f227d3de7a727c0168b8008c18
NekoCarreraDesigns <denvermaas@gmail.com>
2022-06-14 11:44:55 -0600

    added char string, int, decimal int, and bool for console output

--- Net-tutorial/Program.cs
+++ Net-tutorial/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("The current time is " + DateTime.Now);
+Console.WriteLine('a');
+Console.WriteLine(123);
+Console.WriteLine(12.3m);
+Console.WriteLine(true);
+Console.WriteLine(false);
