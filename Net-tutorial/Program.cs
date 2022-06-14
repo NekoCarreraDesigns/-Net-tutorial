088fa5664a84f90ec78e5b1b14f9273d3affd542
NekoCarreraDesigns <denvermaas@gmail.com>
2022-06-14 12:10:25 -0600

    added variable with output

--- Net-tutorial/Program.cs
+++ Net-tutorial/Program.cs
@@ -6,3 +6,11 @@ Console.WriteLine(123);
 Console.WriteLine(12.3m);
 Console.WriteLine(true);
 Console.WriteLine(false);
+
+string firstName;
+firstName = "Candace";
+Console.WriteLine(firstName);
+firstName = "Nick";
+Console.WriteLine(firstName);
+firstName = "Terrick";
+Console.WriteLine(firstName);
