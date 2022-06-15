e438dd9f45ce66504b8f3f90b82afe8d9eb71c30
NekoCarreraDesigns <denvermaas@gmail.com>
2022-06-15 11:10:40 -0600

    added string escapes to program.cs

--- Net-tutorial/Program.cs
+++ Net-tutorial/Program.cs
@@ -1,25 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The current time is " + DateTime.Now);
-Console.WriteLine('a');
-Console.WriteLine(123);
-Console.WriteLine(12.3m);
-Console.WriteLine(true);
-Console.WriteLine(false);
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("The current time is " + DateTime.Now);
+// Console.WriteLine('a');
+// Console.WriteLine(123);
+// Console.WriteLine(12.3m);
+// Console.WriteLine(true);
+// Console.WriteLine(false);
 
-string firstName;
-firstName = "Candace";
-Console.WriteLine(firstName);
-firstName = "Nick";
-Console.WriteLine(firstName);
-firstName = "Terrick";
-Console.WriteLine(firstName);
+// string firstName;
+// firstName = "Candace";
+// Console.WriteLine(firstName);
+// firstName = "Nick";
+// Console.WriteLine(firstName);
+// firstName = "Terrick";
+// Console.WriteLine(firstName);
 
-var message = "Hello and welcome to C#";
-Console.WriteLine(message);
+// var message = "Hello and welcome to C#";
+// Console.WriteLine(message);
 
-var integer = 123;
-Console.WriteLine(integer);
+// var integer = 123;
+// Console.WriteLine(integer);
 
-var decimalNumber = 34.4;
-Console.WriteLine(decimalNumber);
+// var decimalNumber = 34.4;
+// Console.WriteLine(decimalNumber);
+
+Console.WriteLine("Hello\nWorld!");
+Console.WriteLine("Hello\tWorld!");
+Console.WriteLine("hello \"World\"!");
+Console.WriteLine("c:\\source\\repos");
+
+
